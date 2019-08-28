0804217d5a7010051147c07ce13683bd66eec49f
Harun KURT <harunkurt0000@gmail.com>
2019-08-28 23:09:20 +0300

    Create Booleans.dart

---
+++ lib/Booleans.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  /*
+  Booleans
+  @harunlakodla
+   */
+  print("Booleans");
+
+var fullname='';
+
+assert(fullname.isEmpty);
+// isempty diyerek yokluyoruz basılmış ise gibi mantık var
+
+assert(fullname.isNotEmpty);
+
+  
+}
