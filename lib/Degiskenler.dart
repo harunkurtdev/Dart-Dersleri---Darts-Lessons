eb7a66d8e3455cafa1b362cc4079e68285376258
Harun KURT <harunkurt0000@gmail.com>
2019-08-28 23:09:28 +0300

    Create Degiskenler.dart

---
+++ lib/Degiskenler.dart
@@ -0,0 +1,24 @@
+main() {
+  
+var degisken ="Mehraba";
+// yapamıyoruz var ile int tipe 
+//degisken=4;
+// bunu yapınca hata alırız 
+// variable cünkü bir kendi içinde değisiyor 
+//bunun değişken hali olan ise dynamic
+dynamic degisiyor="dinamik olarak değişitğini biliyoruz";
+//dynamic bizim değişkenimizi değiştirir
+print(degisiyor);
+//
+degisiyor=45;
+print(degisiyor);
+//görüıdüğü gibi tipi değişti 
+
+const int a=54;
+//const ise bir statiktir değişemez
+print(a);
+//a=54;
+//değiştirmek istesekte değişemiyor
+
+final sonhaldekidegisken="Bu değişken son halde değiştirlemez..";
+}
