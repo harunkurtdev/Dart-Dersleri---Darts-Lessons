31299cb56e208a4247eac35894b40ffc0138276f
Harun KURT <harunkurt0000@gmail.com>
2019-09-07 14:48:52 +0300

    Create launch.json

---
+++ lib/Functions-Fonksiyon.dart
@@ -0,0 +1,23 @@
+var a3;
+
+main() {
+
+/**
+ * Fonksiyon kullanımı 
+ * @harunlakodla
+ * 
+*/
+
+fonksiyon1(5,3);
+
+print(a3);
+}
+
+
+
+void fonksiyon1(int a1,int a2) {
+
+a3=a1+a2;
+return a3;
+
+}
